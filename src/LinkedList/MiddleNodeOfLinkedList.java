c0776831ff7dccd4d0266303a626de3fefeedf29
kumarsudarshan <ksudarsh@in.ibm.com>
2018-02-06 23:32:20 +0530

    Middle Node of Linked List

---
+++ src/LinkedList/MiddleNodeOfLinkedList.java
@@ -0,0 +1,34 @@
+/*
+    Created by : Kumar Sudarshan
+    Date : 6th Feb 2018
+    Middle Node of Linked List
+ */
+
+package LinkedList;
+
+import java.util.Scanner;
+
+public class MiddleNodeOfLinkedList {
+    static ListNode start = null;
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        start = SingleLinkedList.createLinkedList(start);
+        SingleLinkedList.printLinkedList(start);
+        System.out.println("\n\tMiddle Node : " + middleNodeOfLinkedList(start));
+
+    }
+
+    static int middleNodeOfLinkedList(ListNode head){
+        ListNode fastPtr = head,slowPtr = head;
+        while(slowPtr != null && fastPtr != null){
+                fastPtr = fastPtr.getNext();
+                if(fastPtr != null){
+                    fastPtr = fastPtr.getNext();
+                    slowPtr = slowPtr.getNext();
+                }
+            }
+
+        return slowPtr.getData();
+    }
+}
