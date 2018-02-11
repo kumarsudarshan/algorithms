fd956dd0d760f8239b9cdd6b9aa565fa748b8e79
kumarsudarshan <ksudarsh@in.ibm.com>
2018-02-11 18:49:46 +0530

    Level Order traversal

---
+++ src/Trees/LevelOrderTraversal.java
@@ -0,0 +1,37 @@
+/*
+    Created by : Kumar Sudarshan
+    Date : 11th Feb 2018
+    Level Order Traversal
+ */
+
+package Trees;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+
+    static BinaryTreeNode root = null;
+
+    public static void main(String args[]) {
+        root = BinaryTree.createBinaryTree(root);
+        levelOrderTraversal(root);
+    }
+
+    static void levelOrderTraversal(BinaryTreeNode root) {
+
+        Queue<BinaryTreeNode> queue = new LinkedList<BinaryTreeNode>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            BinaryTreeNode temp = queue.poll();
+            System.out.print("\t" + temp.getData());
+            if (temp.getLeft() != null) {
+                queue.add(temp.getLeft());
+            }
+            if (temp.getRight() != null) {
+                queue.add(temp.getRight());
+            }
+        }
+    }
+}
