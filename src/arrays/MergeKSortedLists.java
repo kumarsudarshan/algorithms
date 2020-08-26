47c1a8915f16360c7b2705db7f3963c49d51cf8b
kumarsudarshan <sudarshansingh9@gmail.com>
2020-08-26 11:09:53 +0530

    added comments

--- src/arrays/MergeKSortedLists.java
+++ src/arrays/MergeKSortedLists.java
@@ -2,6 +2,31 @@ package arrays;
 
 import java.util.PriorityQueue;
 
+/*
+ * 
+ * Given lists of sorted list of integers, merge them into one large sorted list.
+
+Can you do it better than O(kn log kn) where n is the length of the longest list, and k is the number of lists?
+
+Example 1
+Input
+
+lists = [
+    [],
+    [],
+    [10, 12],
+    [],
+    [3, 3, 13],
+    [3],
+    [10],
+    [0, 7]
+]
+Output
+
+[0, 3, 3, 3, 7, 10, 10, 12, 13]
+ * 
+ */
+
 public class MergeKSortedLists {
 	
 	public static void main(String[] args) {
