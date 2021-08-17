f7232505efdac385ead6c0181198913baa4c1b49
kumarsudarshan <ksudarsh@in.ibm.com>
2021-08-17 23:54:55 +0530

    Math

--- README.md
+++ README.md
@@ -305,6 +305,10 @@
 1. [Spiral Order](https://github.com/kumarsudarshan/algorithms/blob/master/src/matrix/SpiralOrder.java)
 2. [Shift in spiral order](https://github.com/kumarsudarshan/algorithms/blob/master/src/matrix/ShiftInSpiralOrder.java)
 
+### Math
+
+1. [Random number generator](https://github.com/kumarsudarshan/algorithms/blob/master/src/math/RandomGeneratorProbability.java)
+
 ## Design
 
 1. [Design LRU Cache](https://github.com/kumarsudarshan/algorithms/blob/master/src/design/LRUCache.java)
---
+++ src/math/RandomGeneratorProbability.java
@@ -0,0 +1,49 @@
+package math;
+
+import java.util.Random;
+
+/*
+A dice is generating random number from 1 to 7,
+We need to modify random number to generate number from 1 to 5 with equal probability
+ */
+public class RandomGeneratorProbability {
+    public static void main(String[] args) {
+        calculateProbability();
+    }
+
+    static int count = 0;
+
+    public static int generate() {
+        int ran1 = new Random().nextInt(7) + 1;
+        int random = 0;
+        if (ran1 < 6) {
+            return ran1;
+        } else {
+            random = 1 + (count++) % 5;
+        }
+        return random;
+    }
+
+    public static int generate1() {
+        int i;
+        int ran1 = new Random().nextInt(7) + 1;
+        int ran2 = new Random().nextInt(7) + 1;
+        int ran3 = new Random().nextInt(7) + 1;
+        int ran4 = new Random().nextInt(7) + 1;
+        i = 7 * ran1 + ran2 + ran3 + ran4;
+        return i % 5 + 1;
+    }
+
+    public static void calculateProbability() {
+        int[] freq = new int[6];
+
+        for (int i = 0; i < 10000000; i++) {
+            int val = generate();
+            freq[val]++;
+        }
+
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i + "   " + freq[i] / 100000.0);
+        }
+    }
+}
